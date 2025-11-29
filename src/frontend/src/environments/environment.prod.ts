679273a7ccce66fde114b7c15ec53a3a019394f0
tien22521469 <163420484+tien22521469@users.noreply.github.com>
2025-11-29 16:33:14 +0700

    Update environment.prod.ts

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8082/api',
-  checkoutUrl: 'http://localhost:8082/api/checkout',
-  userServiceUrl: 'http://localhost:8081/api',
-};
 
+  // Thay localhost:8082 bằng URL của Product Service LoadBalancer (Cổng 80)
+  apiBaseUrl: 'http://a245f9f90c03746d79aec8ce9e63f600-244486027.ap-southeast-1.elb.amazonaws.com/api',
+  
+  checkoutUrl: 'http://a245f9f90c03746d79aec8ce9e63f600-244486027.ap-southeast-1.elb.amazonaws.com/api/checkout',
+
+  // Thay localhost:8081 bằng URL của User Service LoadBalancer (Cổng 80)
+  userServiceUrl: 'http://ac41dc0af274644bc814b33d3a54006f-2094462698.ap-southeast-1.elb.amazonaws.com/api'
+};
